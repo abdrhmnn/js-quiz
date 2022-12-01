aaf9c8cc983e706f7d8560bbfb336297f90a053f
Abdurrahman <Abdrhmn.gh@gmail.com>
2022-12-01 13:22:58 +0700

    finished quiz app

--- index.js
+++ index.js
@@ -42,3 +42,70 @@ let correctCount = 0;
 let wrongCount = 0;
 let total = 0;
 let selectedAnswer;
+
+const playAgain = () => {
+	qIndex = 0;
+	correctCount = 0;
+	wrongCount = 0;
+	total = 0;
+	showQuestion(qIndex);
+};
+
+play.addEventListener("click", () => {
+	resultScreen.style.display = "none";
+	gameScreen.style.display = "block";
+	playAgain();
+});
+
+const showResult = () => {
+	resultScreen.style.display = "block";
+	gameScreen.style.display = "none";
+
+	resultScreen.querySelector(".correct").textContent = `Correct Answers: ${correctCount}`;
+
+	resultScreen.querySelector(".wrong").textContent = `Wrong Answers: ${wrongCount}`;
+
+	resultScreen.querySelector(".score").textContent = `Score: ${
+		(correctCount - wrongCount) * 10
+	}`;
+};
+
+const showQuestion = (qNumber) => {
+	if (qIndex === dataQuiz.length) return showResult();
+	selectedAnswer = null;
+	question.textContent = dataQuiz[qNumber].question;
+	answersContainer.innerHTML = dataQuiz[qNumber].answers
+		.map(
+			(item, index) =>
+				`
+      <div class="answer">
+          <input type="radio" id=${index} name="answer" value=${item.isCorrect} />
+          <label for="1">${item.answer}</label>
+      </div>
+    `
+		)
+		.join("");
+
+	selectAnswer();
+};
+
+const selectAnswer = () => {
+	answersContainer.querySelectorAll("input").forEach((el) => {
+		el.addEventListener("click", (e) => {
+			selectedAnswer = e.target.value;
+		});
+	});
+};
+
+const submitAnswer = () => {
+	submit.addEventListener("click", () => {
+		if (selectedAnswer !== null) {
+			selectedAnswer === "true" ? correctCount++ : wrongCount++;
+			qIndex++;
+			showQuestion(qIndex);
+		} else alert("Select an answer!");
+	});
+};
+
+showQuestion(qIndex);
+submitAnswer();
